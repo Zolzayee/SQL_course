2d341ee8355d2d57bc2a6b79d5a9335213bdb62d
Zolzaya Enkhbold <zoya.enkhbold@gmail.com>
2025-07-02 14:31:27 +0700

    commit change

--- SQL_course_Project/01_Top_paying_jobs.sql
+++ SQL_course_Project/01_Top_paying_jobs.sql
@@ -1,4 +1,5 @@
 --Top 10 highest paying data analyst roles that are either remote or local
+-- basic query #01 
 SELECT
 	job_id,
 	job_title,
@@ -6,10 +7,28 @@ SELECT
 	job_schedule_type,
 	salary_year_avg,
 	job_posted_date
-	-- name AS company_name
 FROM
 	job_postings_fact
--- LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+	job_title = 'Data Analyst'
+	AND salary_year_avg IS NOT NULL
+	AND job_location = 'Anywhere'
+ORDER BY
+	salary_year_avg DESC 
+LIMIT 10;
+
+-- query #02 
+SELECT
+	job_id,
+	name AS company_name,
+	job_title,
+	job_location,
+	job_schedule_type,
+	salary_year_avg,
+	job_posted_date
+FROM
+	job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
 WHERE
 	job_title = 'Data Analyst'
 	AND salary_year_avg IS NOT NULL
