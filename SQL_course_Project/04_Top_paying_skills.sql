0e74974b9e351dfc9a737a32a58eb9d636667169
Zolzaya Enkhbold <zoya.enkhbold@gmail.com>
2025-07-02 21:01:35 +0700

    commit changes

--- SQL_course_Project/04_Top_paying_skills.sql
+++ SQL_course_Project/04_Top_paying_skills.sql
@@ -1,7 +1,7 @@
 -- Calculates the average salary for job postings by individual skill 
 SELECT
   skills_dim.skills AS skill, 
-  ROUND(AVG(job_postings_fact.salary_year_avg),0) AS avg_salary
+  ROUND(AVG(job_postings_fact.salary_year_avg),0) AS avg_salary -- round up function was newly used here. 
 FROM
   job_postings_fact
 	INNER JOIN
@@ -17,3 +17,22 @@ GROUP BY
 ORDER BY
   avg_salary DESC
 LIMIT 20; 
+
+/*
+Top Paying Skills – Key Insights
+
+Niche = High Pay
+Rare skills like svn and solidity command top salaries due to low supply and specialized use (e.g., legacy systems, blockchain).
+
+AI & ML Frameworks Are Lucrative
+Tools like pytorch, keras, hugging face, and tensorflow reflect strong demand for deep learning and NLP expertise.
+
+DevOps & Data Infrastructure Skills Boost Value
+Knowledge of terraform, ansible, puppet, gitlab, and kafka signals production-level data maturity—highly rewarded.
+
+AutoML & Hybrid Skills Matter
+Platforms like datarobot and tools like dplyr show value in combining automation with analytical rigor.
+
+Common ≠ Highest Paying
+Skills like SQL, Excel, and Power BI are widely required but don’t differentiate on pay—specialize to stand out.
+*/
